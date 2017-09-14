faa04a45bbb17d6417b8761f30c9378f959900f0
maverck- <maverick.gayoso@gmail.com>
2017-09-13 21:30:17 -0300

    .

---
+++ ProyectoBanco/src/tipoInv/Monto.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipoInv;
+
+import java.util.ArrayList;
+import proyectobanco.IOEstrategia;
+
+/**
+ *
+ * @author Maver
+ */
+public class Monto extends GestorDatos {
+
+    @Override
+    public String[][] informe() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList Filtro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IOEstrategia getArchivo() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
